875da34c4e2dfc0c39949fdbad1117d7004c3b12
ytemiloluwa <ytemiloluwa@gmail.com>
2024-10-29 15:15:22 +0100

    Appstate: wrap connection to arc pointer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::net::TcpListener;
 use NewsLetterAPI::startup::run;
 use NewsLetterAPI::configuration::get_configuration;
+use sqlx:: {Connection, PgConnection};
 
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), std::io::Error> {
     // Panic if it cant read configuration file
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,16 +2,20 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    //let db_pool = Data::new(db_pool);
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+
+    // wrap the connection to a smart pointer
+    let connection = web::Data::new(connection);
+    // capture `connection` from the surr
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            //.app_data(db_pool.clone())
+            // get a pointer copy and attach it to application state.
+            .app_data(connection)
     })
         .listen(listener)?
         .run();
