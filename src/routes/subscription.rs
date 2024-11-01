45d7140f7ba46d133a979c57306a49e4358a113b
ytemiloluwa <ytemiloluwa@gmail.com>
2024-11-01 21:05:05 +0100

    sqlx: insert query

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -12,8 +12,19 @@ pub struct FormData {
 
 // return a 200 response
 
-pub async fn subscribe(_form : web::Form<FormData>,
+pub async fn subscribe(form : web::Form<FormData>,
                        // retrieving a connection from the application state
-_connection: web::Data<PgConnection>, ) -> HttpResponse {
+connection: web::Data<PgConnection>, ) -> HttpResponse {
+
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+
+    "#, Uuid::new_v4(), form.email, form.name, Utc::now())
+        // use get_ref to get an immutable reference to the PgConnection
+        // wrapped by 'web::Data'
+        .execute(connection.get_ref())
+        .await;
     HttpResponse::Ok().finish()
 }
