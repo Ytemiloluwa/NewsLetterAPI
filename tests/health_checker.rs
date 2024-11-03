a7ef283a8428d12a057de50208fa4bdc6a05e5c9
ytemiloluwa <ytemiloluwa@gmail.com>
2024-11-03 22:31:13 +0100

    Test isolation

--- tests/health_checker.rs
+++ tests/health_checker.rs
@@ -1,9 +1,7 @@
-use std::fmt::format;
 use std::net::TcpListener;
-use NewsLetterAPI::startup::run;
-use sqlx:: PgPool;
-use NewsLetterAPI::configuration::get_configuration;
-
+use sqlx:: {Connection, Executor, PgConnection, PgPool} ;
+use NewsLetterAPI::configuration:: { get_configuration, DatabaseSettings};
+use uuid::Uuid;
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
@@ -14,10 +12,10 @@ async fn spawn_app() -> TestApp {
     //  retrieve the port assigned by the OS
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres. ");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
+
     let server = NewsLetterAPI::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
@@ -25,6 +23,24 @@ async fn spawn_app() -> TestApp {
         address,
         db_pool: connection_pool,
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool { // Create database
+    let mut connection = PgConnection::connect(
+        &config.connection_string_without_db()
+    )
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str()) .await
+        .expect("Failed to create database.");
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string()) .await
+        .expect("Failed to connect to Postgres."); sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+    connection_pool
 }
 
 #[tokio::test]
