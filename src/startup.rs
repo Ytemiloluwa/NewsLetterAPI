abd1f5966640fc9703d436e00c141664f701079e
ytemiloluwa <ytemiloluwa@gmail.com>
2024-11-06 18:01:29 +0100

    log: added Middleware on app

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -12,6 +13,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // capture `connection` from the surr
     let server = HttpServer::new(move || {
         App::new()
+            // Added middleware using the wrap method on the App
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // get a pointer copy and attach it to application state.
