24d9d1ed0ad4d112e60d8edceef00812032d34e5
ytemiloluwa <ytemiloluwa@gmail.com>
2024-11-09 15:44:14 +0100

    Added tracing-subscriber to main

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,25 @@ use NewsLetterAPI::startup::run;
 use NewsLetterAPI::configuration::get_configuration;
 use sqlx::PgPool;
 use env_logger::Env;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter:: { BunyanFormattingLayer, JsonStorageLayer };
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info")); let formatting_layer = BunyanFormattingLayer::new(
+        "NewsLetterAPI".into(),
+        std::io::stdout
+
+    );
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     // Panic if it cant read configuration file
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
