dd83ccfe466a66b63d514e3c2dd3acbc9fb67f16
ytemiloluwa <ytemiloluwa@gmail.com>
2024-11-09 20:34:40 +0100

    console: restore actix-web logs

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::net::TcpListener;
 use NewsLetterAPI::startup::run;
 use NewsLetterAPI::configuration::get_configuration;
 use sqlx::PgPool;
-use env_logger::Env;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter:: { BunyanFormattingLayer, JsonStorageLayer };
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Redirect all log's event to our subscriber
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info")); let formatting_layer = BunyanFormattingLayer::new(
         "NewsLetterAPI".into(),
