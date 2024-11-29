6a14286bc95db8d4cc53693aed8d55d69cc06aed
Ytemiloluwa <ytemiloluwa@gmail.com>
2024-11-29 23:59:30 +0100

    updated main

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@ use std::net::TcpListener;
 use newsletterapi::startup::run;
 use newsletterapi::configuration::get_configuration;
 use newsletterapi::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::{PgConnectOptions, PgPool};
+use sqlx::postgres::PgPoolOptions;
 use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
 
-    let subscriber = get_subscriber("NewsLetterAPI".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("newsletterapi".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     // Panic if it cant read configuration file
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgConnectOptions::new()
+    let connection_pool = PgPoolOptions::new()
         .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
