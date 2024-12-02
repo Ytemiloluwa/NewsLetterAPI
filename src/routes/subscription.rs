0e3a3c91a0040dda57b36ed7896aff20da967f10
Ytemiloluwa <ytemiloluwa@gmail.com>
2024-12-02 22:01:09 +0100

    updated subscription for ownership and invariant

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -1,9 +1,8 @@
+use crate::domain::{NewSubscriber, SubscriberName};
 use actix_web::{web, HttpResponse};
-use actix_web::web::Form;
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
-use tracing::Instrument;
 use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(serde::Deserialize)]
@@ -23,36 +22,34 @@ pub struct FormData {
 
 // return a 200 response
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    if !is_valid_name(&form.name) {
-        return HttpResponse::BadRequest().finish();
-    }
-    match insert_subscriber(&pool, &form).await
+    let name = match SubscriberName::parse(form.0.name.clone()) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let new_subscriber = NewSubscriber {
+        email: form.0.email.clone(),
+        name,
+    };
+    match insert_subscriber(&pool, &new_subscriber).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish()
     }
 }
-
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-    let is_too_long = s.graphemes(true).count() > 256;
-    let forbidden_characters = ['/', '(', ')', '"' , '<', '>', '\\', '{', '}'];
-    let contains_forbidden_characters = s
-        .chars()
-        .any(|g| forbidden_characters.contains(&g));
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
-}
-
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!( r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
     "#,
-        Uuid::new_v4(), form.email, form.name, Utc::now())
+        Uuid::new_v4(),
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
+        Utc::now()
+        )
         .execute(pool)
         .await
         .map_err(|e| {
